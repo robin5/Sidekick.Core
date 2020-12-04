45952d5f366b74bca1f3e561873e2e9538f8d976
Robin Murray <robin.murray360@gmail.com>
2020-12-03 22:24:21 -0800

    Adds Sidekick.Test test project

---
+++ Sidekick.Test/HomeControllerTest.cs
@@ -0,0 +1,30 @@
+using System;
+using Xunit;
+
+namespace Sidekick.Test
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void GetIndex()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+        [Fact]
+        public void GetPrivacy()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+        [Fact]
+        public void GetError()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+    }
+}
